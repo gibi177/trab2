e2d34127c76f98e859ae01beba54543c25dc1bd5
felipe gibin <gibinf.duarte@gmail.com>
2024-05-09 14:56:33 -0300

    testcase 2 passa

--- testa_rainha.cpp
+++ testa_rainha.cpp
@@ -7,3 +7,9 @@ TEST_CASE("Entrada inválida, não é 8x8", "[rainha]") {
     REQUIRE(answer("entradas/testcase1/teste2.txt") == -1);
     REQUIRE(answer("entradas/testcase1/teste3.txt") == -1);
 }
+
+TEST_CASE("Entrada inválida, caractere n binario", "[rainha]") {
+    REQUIRE(answer("entradas/testcase2/teste4.txt") == -1);
+    REQUIRE(answer("entradas/testcase2/teste5.txt") == -1);
+    REQUIRE(answer("entradas/testcase2/teste6.txt") == -1);
+}
