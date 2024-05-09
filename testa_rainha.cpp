e296fdbb3c8e186c97319d4d4f0060518fe7a037
felipe gibin <gibinf.duarte@gmail.com>
2024-05-09 14:59:57 -0300

    testecase3 falha

--- testa_rainha.cpp
+++ testa_rainha.cpp
@@ -13,3 +13,9 @@ TEST_CASE("Entrada inválida, caractere n binario", "[rainha]") {
     REQUIRE(answer("entradas/testcase2/teste5.txt") == -1);
     REQUIRE(answer("entradas/testcase2/teste6.txt") == -1);
 }
+
+TEST_CASE("Entrada inválida, numero de rainhas diferente de 8", "[rainha]") {
+    REQUIRE(answer("entradas/testcase3/teste7.txt") == -1);
+    REQUIRE(answer("entradas/testcase3/teste8.txt") == -1);
+    REQUIRE(answer("entradas/testcase3/teste9.txt") == -1);
+}
