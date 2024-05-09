1d9f6deb9b5607f4989b90f201d9cab6f54925a6
felipe gibin <gibinf.duarte@gmail.com>
2024-05-09 14:51:47 -0300

    cria mais teste para testcase 1

--- testa_rainha.cpp
+++ testa_rainha.cpp
@@ -4,4 +4,6 @@
 
 TEST_CASE("Entrada inválida, não é 8x8", "[rainha]") {
     REQUIRE(answer("entradas/testcase1/teste1.txt") == -1);
+    REQUIRE(answer("entradas/testcase1/teste2.txt") == -1);
+    REQUIRE(answer("entradas/testcase1/teste3.txt") == -1);
 }
