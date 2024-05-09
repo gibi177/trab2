9350d564fdef56f713cfe4acae109e5d31a5ef83
felipe gibin <gibinf.duarte@gmail.com>
2024-05-09 14:47:28 -0300

    passa testcase 1

--- rainha.cpp
+++ rainha.cpp
@@ -9,3 +9,36 @@ using std::endl;
 using std::string;
 
 
+int isBinary8x8 (const std::string& filename) {
+    std::ifstream file(filename);
+    string line;
+    int linecounter = 0;
+
+    if (!file) {
+        std::cout << "Erro ao abrir o arquivo." << filename << std::endl;
+        return -1;
+    } 
+    
+    while (getline(file, line)) {
+        if (line.size() != 8) {
+            return -1;
+        }
+        for (char c : line) {
+            if (c != '0' && c != '1') {
+                return -1;
+            }
+        }   
+        linecounter ++;
+    }
+
+    file.close();
+    if (linecounter != 8) {
+        return -1;
+    }
+
+    return 1;
+}
+
+int answer(const string& filename) {
+    return isBinary8x8(filename);
+}
