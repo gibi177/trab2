f97d7063d0c16351d671ff9c85eebe5ca5e68d56
felipe gibin <gibinf.duarte@gmail.com>
2024-05-09 21:17:09 -0300

    alguns comentarios

--- rainha.cpp
+++ rainha.cpp
@@ -19,16 +19,18 @@ using std::abs;
 //vetor global que armazena as coordenadas das rainhas
 vector <pair <int, int>> positions;
 
+
+//Checa se o input é matriz 8x8, se tem 8 rainhas, se a matriz é binaria e cria vetor com as coordenadas das rainhas
 int isValid (const std::string& filename) {
     std::ifstream file(filename);
     string line;
     int linecounter = 0;
     int queencounter = 0;
-    positions.clear();
+    positions.clear();   //cada vez q a funcao for chamada o vetor é limpado para ficar vazio
 
     if (!file) {
         std::cout << "Erro ao abrir o arquivo." << filename << std::endl;
-        return -1;
+        return -2;
     } 
     
     while (getline(file, line)) {
@@ -59,6 +61,7 @@ int isValid (const std::string& filename) {
     return 1;
 }
 
+//Checa se as rainhas se atacam na horizontal, vertical e diagonal
 int isAttacked (const vector<pair<int, int>>& positions) {
     for (std::size_t i = 0; i < positions.size(); i++) {
         for (std::size_t j = i + 1; j < positions.size(); j++) {
@@ -71,6 +74,7 @@ int isAttacked (const vector<pair<int, int>>& positions) {
     return 0;
 }
 
+//Usa as funcoes de cima para retornar a resposta final
 int answer(const string& filename) {
     int valid = isValid(filename);
 
