b8decba320acb766075550da0753e7f89c2f85be
felipe gibin <gibinf.duarte@gmail.com>
2024-05-15 16:37:01 -0300

    cpplint style

--- rainha.cpp
+++ rainha.cpp
@@ -8,39 +8,39 @@
 #include <cmath>       //para usar std::abs
 #include <set>
 
-using namespace std;
+using namespace std;   // Pra n precisar botar ::std 
 
-//vetor global que armazena as coordenadas das rainhas
+// Vetor global que armazena as coordenadas das rainhas
 vector <pair <int, int>> positions;
 
 
-//Checa se o input é matriz 8x8, se tem 8 rainhas, se a matriz é binaria e cria vetor com as coordenadas das rainhas
-int isValid (const std::string& filename) {
+// Checa se o input é matriz 8x8, se tem 8 rainhas, se a matriz é binaria e cria vetor com as coordenadas das rainhas
+int isValid(const std::string& filename) {
     ifstream file(filename);
     string line;
     int linecounter = 0;
     int queencounter = 0;
-    positions.clear();   //cada vez q a funcao for chamada o vetor é limpado para ficar vazio
+    positions.clear();   // Cada vez q a funcao for chamada o vetor é limpado para ficar vazio
 
-    if (!file) {   //arquivo não abriu
+    if (!file) {   // Arquivo não abriu
         return -2;
-    } 
-    
+    }
+   
     while (getline(file, line)) {
         if (line.size() != 8) {
             return -1;
         }
         for (int i = 0; i < 8; i++) {
             char c = line[i];
-            if (c != '0' && c != '1') {   //entrada da matriz nao e binaria
+            if (c != '0' && c != '1') {   // Entrada da matriz nao e binaria
                 return -1;
             }
             if (c == '1') {
                 queencounter++;
                 positions.push_back({linecounter, i});
             }
-        }   
-        linecounter ++;
+        } 
+        linecounter++;
     }
 
     file.close();
@@ -54,26 +54,26 @@ int isValid (const std::string& filename) {
     return 1;
 }
 
-//Checa se as rainhas se atacam na horizontal, vertical e diagonal
-bool isAttacked (const vector<pair<int, int>>& positions) {
+// Checa se as rainhas se atacam na horizontal, vertical e diagonal
+bool isAttacked(const vector<pair<int, int>>& positions) {
     static int counter = 1;
     bool isunderattack = false;
 
-    //cria arquivo com as rainhas que se atacam nesse teste
-    //note que se a matriz for solucao, cria um arquivo que ficara vazio (sem ataques)
+    // Cria arquivo com as rainhas que se atacam nesse teste
+    // Note que se a matriz for solucao, cria um arquivo que ficara vazio (sem ataques)
     string ataquesfile = "ataques/atck" + to_string(counter);
     ofstream createdfile(ataquesfile);
 
     for (std::size_t i = 0; i < positions.size(); i++) {
         for (std::size_t j = i + 1; j < positions.size(); j++) {
 
-            //Todos os casos que as rainhas se atacam
+            // Todos os casos que as rainhas se atacam
             if (positions[i].first == positions[j].first || positions[i].second == positions[j].second ||
                 abs(positions[i].first - positions[j].first) == abs(positions[i].second - positions[j].second)) {
 
                 if (createdfile.is_open()) {
                     createdfile << positions[i].first << "," << positions[i].second << " "
-                                << positions[j].first << "," << positions[j].second << endl;   
+                                << positions[j].first << "," << positions[j].second << endl; 
                 } else {
                     cout << "falha ao criar arquivo" << endl;
                 }
@@ -81,18 +81,18 @@ bool isAttacked (const vector<pair<int, int>>& positions) {
             }
         }
     }
-    counter ++;
-    createdfile.close();   //fecha arquivo
+    counter++;
+    createdfile.close();   // Fecha arquivo
     return isunderattack;
 }
 
-//Usa as funcoes de cima para retornar a resposta final
+// Usa as funcoes de cima para retornar a resposta final
 int answer(const string& filename) {
     int valid = isValid(filename);
 
     if (valid != 1) {
         return valid;
-    } 
+    }
 
     bool isunderattack = isAttacked(positions);
     if (isunderattack) {
--- rainha.hpp
+++ rainha.hpp
@@ -1,14 +1,14 @@
-#include <fstream>      
+#include <fstream>  
 #include <string>
 #include <iostream>
 #include <vector>
-#include <utility>     
-#include <cstddef>    
-#include <cmath>       
+#include <utility>
+#include <cstddef>
+#include <cmath>
 
-using std::cout; 
+using std::cout;
 using std::cin;
-using std::endl; 
+using std::endl;
 using std::string;
 using std::vector;
 using std::pair;
